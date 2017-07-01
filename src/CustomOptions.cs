1f87db9d7bd35fd6abe0e917b8a761170a9ad02e
Daniel Sandberg <elicit.daniel@outlook.com>
2017-07-01 21:54:13 +0200

    Added CustomControllerProvider Added CustomOptions

---
+++ src/CustomOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Shell;
+
+namespace DialToolsForVS
+{
+    [Guid("6C43E501-226F-4E73-949C-BF8808A94B48")]
+    public class CustomOptions : DialogPage
+    {
+        private string clickAction = string.Empty;
+        private string rightAction = string.Empty;
+        private string leftAction = string.Empty;
+
+        public string ClickAction { get => clickAction; set => clickAction = value; }
+        public string RightAction { get => rightAction; set => rightAction = value; }
+        public string LeftAction { get => leftAction; set => leftAction = value; }
+
+        protected override IWin32Window Window
+        {
+            get
+            {
+                CustomOptionsControl page = new CustomOptionsControl();
+                page.optionsPage = this;
+                page.Initialize();
+                return page;
+            }
+        }
+
+    }
+}
---
+++ src/Providers/Custom/CustomControllerProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace DialToolsForVS
+{
+    [DialControllerProvider(order: 9)]
+    internal class CustomControllerProvider : IDialControllerProvider
+    {
+        public static string Moniker = KnownProviders.Custom.ToString();
+
+        public IDialController TryCreateController(IDialControllerHost host)
+        {
+            string iconFilePath = VsHelpers.GetFileInVsix("Providers\\Custom\\icon.png");
+            host.AddMenuItem(Moniker, iconFilePath);
+
+            return new CustomController();
+        }
+    }
+}
